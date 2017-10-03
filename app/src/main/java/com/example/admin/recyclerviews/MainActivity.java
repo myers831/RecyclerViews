c594c951c6e0e2968429ef53d71b373cbdc9faaa
Mac <myers831@yahoo.com>
2017-10-03 18:06:33 -0400

    Initial commit

---
+++ app/src/main/java/com/example/admin/recyclerviews/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.admin.recyclerviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvPersonList;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.ItemAnimator itemAnimator;
+    private List<Person> personList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvPersonList = (RecyclerView) findViewById(R.id.rvPersonList);
+
+        personList = new ArrayList<>();
+        personList.add(new Person("Manny", "28"));
+        personList.add(new Person("Mac", "24"));
+        personList.add(new Person("Jason", "24"));
+        personList.add(new Person("Joseph", "24"));
+        personList.add(new Person("Jarett", "27"));
+        personList.add(new Person("Ryen", "29"));
+        personList.add(new Person("Manny", "28"));
+        personList.add(new Person("Mac", "24"));
+        personList.add(new Person("Jason", "24"));
+        personList.add(new Person("Joseph", "24"));
+        personList.add(new Person("Jarett", "27"));
+        personList.add(new Person("Ryen", "29"));
+
+        layoutManager = new LinearLayoutManager(this);
+        itemAnimator = new DefaultItemAnimator();
+        PersonListAdapter personListAdapter = new PersonListAdapter(personList);
+
+        rvPersonList.setLayoutManager(layoutManager);
+        rvPersonList.setItemAnimator(itemAnimator);
+        rvPersonList.setAdapter(personListAdapter);
+
+
+    }
+}
